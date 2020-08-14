18721841e80d1874b0e0dca080d29b435e75ff5d
hyeonjin <ki1029m@naver.com>
2020-08-14 21:07:53 +0900

    url 미디어 세팅 & 페이지 대략적인 분배

--- contest/urls.py
+++ contest/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 import contest.views
 
 urlpatterns = [
     path('', contest.views.home, name="home"),
+    path('contestPost/', contest.views.contestPost, name="contestPost"),
+    path('hostPage/', contest.views.hostPage, name="hostPage"),
+    path('participantPage/', contest.views.participantPage, name="participantPage"),
 ]
+
--- contest/views.py
+++ contest/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
+from .models import *
 
 # Create your views here.
+
+#C
+
+#R
+
+#홈페이지
 def home(request):
     return render(request,'home.html')
+
+#공모전 게시글
+def contestPost(request):
+    return render(request,'contestPost.html')
+
+#공모전 개최자 페이지
+def hostPage(request):
+    return render(request,'hostPage.html')
+
+#공모전 참여자 페이지
+def participantPage(request):
+    return render(request,'participantPage.html')
+#U
+
+#D
