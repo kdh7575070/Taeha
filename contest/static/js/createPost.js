28b88fd300628a82694ad0cb85d7fc10cd572b7d
hyeonjin <ki1029m@naver.com>
2020-09-06 13:14:06 +0900

    검색버튼 오류 해결

--- contest/static/js/createPost.js
+++ contest/static/js/createPost.js
@@ -1,6 +1,6 @@
 alert("준비중...");
-// $(document).ready(function () {
-//     $("#comment").click(function () {
-//         alert('....');
-//     });
-// });
+$(document).ready(function () {
+     $("#comment").click(function () {
+         alert('....');
+    });
+});
